67b1c2f7f577088678df00ede69318f62fd27145
ANKITSAINI498 <sainikumar.in@gmail.com>
2026-09-10 14:49:37 +0530

    Auto-commit: src/utils/dateUtils.js at 10/09/2026, 14:49:37

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,2 +1,9 @@
-export const nowLabel = () => new Intl.DateTimeFormat('en-IN', { hour: 'numeric', minute: '2-digit' }).format(new Date());
-export const todayLabel = () => new Intl.DateTimeFormat('en-IN', { day: '2-digit', month: 'short' }).format(new Date());
+export const nowLabel = () =>
+  new Intl.DateTimeFormat("en-IN", {
+    hour: "numeric",
+    minute: "2-digit",
+  }).format(new Date());
+export const todayLabel = () =>
+  new Intl.DateTimeFormat("en-IN", { day: "2-digit", month: "short" }).format(
+    new Date(),
+  );
