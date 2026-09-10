b10ced8ea22d652366a0bd8cd4318356b09053a1
ANKITSAINI498 <sainikumar.in@gmail.com>
2026-09-10 14:37:33 +0530

    Auto-commit: src/services/api.js, src/services/chatService.js at 10/09/2026, 14:37:33

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -1,3 +1,3 @@
-import {api} from './api';
+import { api } from './api';
 import data from '../data/chatData.json';
-export async function fetchChats(){try{const r=await api.get('/chats');return r.data}catch{return {data,offline:true}}}
+export async function fetchChats() { try { const r = await api.get('/chats'); return r.data } catch { return { data, offline: true } } }
