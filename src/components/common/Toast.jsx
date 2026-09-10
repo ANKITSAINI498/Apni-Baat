64c698d64fd7ad345c65dde8d8f782bf64fd3967
ANKITSAINI498 <sainikumar.in@gmail.com>
2026-09-10 14:34:20 +0530

    Auto-commit: src/components/common/Toast.jsx at 10/09/2026, 14:34:20

--- src/components/common/Toast.jsx
+++ src/components/common/Toast.jsx
@@ -1,1 +1,19 @@
-import {AnimatePresence,motion} from 'framer-motion';import {useChat} from '../../context/ChatContext';export default function Toast(){const {toast}=useChat();return <AnimatePresence>{toast&&<motion.div initial={{y:-20,opacity:0}} animate={{y:0,opacity:1}} exit={{y:-20,opacity:0}} className="toastx">● {toast}</motion.div>}</AnimatePresence>}
+import { AnimatePresence, motion } from "framer-motion";
+import { useChat } from "../../context/ChatContext";
+export default function Toast() {
+  const { toast } = useChat();
+  return (
+    <AnimatePresence>
+      {toast && (
+        <motion.div
+          initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -20, opacity: 0 }}
+          className="toastx"
+        >
+          ● {toast}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
