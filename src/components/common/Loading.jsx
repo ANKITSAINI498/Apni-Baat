5f15a35b89e2e1dcf22ffc84a393e239447dfcce
ANKITSAINI498 <sainikumar.in@gmail.com>
2026-09-10 14:34:15 +0530

    Auto-commit: src/components/common/Loading.jsx, src/components/common/Modal.jsx at 10/09/2026, 14:34:15

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,1 +1,9 @@
-export default function Loading(){return <div className="loading"><span/><span/><span/></div>}
+export default function Loading() {
+  return (
+    <div className="loading">
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+}
