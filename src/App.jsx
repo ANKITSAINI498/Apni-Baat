2bedc3552446db5e98e45516a41b660281f21a4d
ANKITSAINI498 <sainikumar.in@gmail.com>
2026-09-10 14:38:36 +0530

    Auto-commit: src/App.jsx at 10/09/2026, 14:38:36

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,29 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';import {ThemeProvider} from './context/ThemeContext';import {ChatProvider} from './context/ChatContext';import {AuthProvider} from './context/AuthContext';import Home from './pages/Home';import Profile from './pages/Profile';import Settings from './pages/Settings';import Favorites from './pages/Favorites';import Archived from './pages/Archived';export default function App(){return <AuthProvider><ThemeProvider><ChatProvider><BrowserRouter><Routes><Route path="/" element={<Home/>}/><Route path="/profile" element={<Profile/>}/><Route path="/settings" element={<Settings/>}/><Route path="/favorites" element={<Favorites/>}/><Route path="/archived" element={<Archived/>}/><Route path="*" element={<Home/>}/></Routes></BrowserRouter></ChatProvider></ThemeProvider></AuthProvider>}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import { ChatProvider } from "./context/ChatContext";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import Favorites from "./pages/Favorites";
+import Archived from "./pages/Archived";
+export default function App() {
+  return (
+    <AuthProvider>
+      <ThemeProvider>
+        <ChatProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/archived" element={<Archived />} />
+              <Route path="*" element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+        </ChatProvider>
+      </ThemeProvider>
+    </AuthProvider>
+  );
+}
