907a27583f5d13b6d029be387f95beec9ec7f466
ANKITSAINI498 <sainikumar.in@gmail.com>
2026-09-10 14:36:49 +0530

    Auto-commit: src/hooks/useLocalStorage.js at 10/09/2026, 14:36:49

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,3 +1,9 @@
-import { useEffect, useState } from 'react';
-import { loadState, saveState } from '../utils/storage';
-export default function useLocalStorage(key, initial) { const [value, setValue] = useState(() => loadState(key, initial)); useEffect(() => { saveState(key, value) }, [key, value]); return [value, setValue] }
+import { useEffect, useState } from "react";
+import { loadState, saveState } from "../utils/storage";
+export default function useLocalStorage(key, initial) {
+  const [value, setValue] = useState(() => loadState(key, initial));
+  useEffect(() => {
+    saveState(key, value);
+  }, [key, value]);
+  return [value, setValue];
+}
